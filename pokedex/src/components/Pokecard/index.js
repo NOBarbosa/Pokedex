4c226b77659bbd37e5047427658f8a2ca6f6e3b4
Natália Barbosa <nob.ufba@gmail.com>
2021-11-14 12:38:28 -0300

    Add hover no card.

--- pokedex/src/components/Pokecard/index.js
+++ pokedex/src/components/Pokecard/index.js
@@ -1,39 +1,54 @@
 import React from 'react';
+import './style.css';
 import { makeStyles } from '@material-ui/core/styles';
-
 import Card from '@material-ui/core/Card';
-
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-
 import Typography from '@material-ui/core/Typography';
+import Color from 'color';
+import { Backdrop } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
+    actionArea: {
+        borderRadius: 16,
+        transition: '0.2s',
+        '&:hover': {
+          transform: 'scale(1.1)',
+        },
+      },
   root: {
     width: 300,
     display: 'flex',
     flexDirection: 'column',
     textAlign: 'center',
-    
-   
-    // // justifyContent: 'center'
+    cursor: 'pointer',
+    '&:hover': {
+        boxShadow: `0 6px 12px 0 ${Color(`gray`)
+          .rotate(-12)
+          .darken(0.2)
+          .fade(0.5)}`,
+      },      
   },
+  
+  
   media: {
     height: 'auto',
     paddingTop: '65.25%', // 16:9
     paddingBottom: '30.25%'
   }
-  }
-));
+  
+  }));
 
 export default function Pokecard({name, id, type, image}) {
   const classes = useStyles();
   
   return (
-    <Card className={classes.root}>
+    <Card className={classes.root }  >
         <Typography>
-          #0{id}
+          <strong>
+            #0{id}
+          </strong>
         </Typography>
       <CardMedia
         className={classes.media}
@@ -41,11 +56,12 @@ export default function Pokecard({name, id, type, image}) {
         title={type}
       />
       <CardContent>
-        <Typography variant="body2" color="primary" component="p"
-
+        <Typography variant="body2" component="p"
         >
-          {name} <br></br>
-          {type}
+            <strong>
+                Name: {name} <br></br>
+                Type: {type}
+            </strong>
         </Typography>
       </CardContent>
       
@@ -60,7 +76,7 @@ export default function Pokecard({name, id, type, image}) {
 
 
 
-// import './style.css';
+
 // const Pokecard = ({id, name, type, image}) =>{
 //     return(
 //         <div className="pokecard">
